2d77b4d03c65f80aed8c0ec431f9af57fc1a42c5
Alim <alim-malikov@mail.ru>
2024-11-04 19:18:09 +0300

    created and added new task

---
+++ Task_2/src/Main.java
@@ -0,0 +1,41 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+
+
+
+
+
+public class Main {
+
+    public static void Sum(double a, double b){
+        double s = a + b;
+        System.out.println("Сумма двух чисел a и b равна:" + s);
+    }
+    public static void Multiplicate (double a, double b){
+        double m = a * b;
+        System.out.println("Произведение двух чисел a и b равна:" + m);
+    }
+    public static void Deduct (double a, double b){
+        double d = a - b;
+        System.out.println("Разность двух чисел a и b равна:" + d);
+    }
+    public static void Divide (double a, double b){
+        double D = a / b;
+        System.out.println("Частное двух чисел a и b равна:" + D);
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("Введите два числа a и b: ");
+        Scanner scanner = new Scanner(System.in);
+        double a = scanner.nextDouble();
+        double b = scanner.nextDouble();
+        Sum(a,b);
+        Multiplicate(a,b);
+        Deduct(a,b);
+        Divide(a,b);
+
+    }
+}
